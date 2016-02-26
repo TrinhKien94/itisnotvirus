027a7e26dad2492512b2809ddd048f26f2f1596a
khanhtc3010 <khanhtran0230@gmail.com>
2016-02-26 23:29:28 +0700

    flow code ver 1.0

--- idea.c
+++ idea.c
@@ -1,20 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 #include <string.h>
-#include <wchar.h>
-int main(){
-	FILE *f = fopen("test.c","w"),*g;
-	char s[256];
-	fprintf(f,"#include <stdio.h>\n#include <stdlib.h>\nint main(){\nprintf(\"Hello Boss\\n\")}");
+
+void formatString(char* string){
+	int i = 0;
+	while(string[i] != '\0'){
+		string[i] = '\0';
+		i++;
+	}
+}
+
+void createFileName(char* fileName, int number){
+	char nName[20];
+	sprintf(nName, "%d", number);
+	strcpy(fileName,"codes/test");
+	strcat(fileName,nName);
+	strcat(fileName,".c");
+}
+
+void genFile(char* sourceFileName){
+	FILE *f = fopen(sourceFileName,"w");
+	fprintf(f,"#include <stdio.h>\n#include <stdlib.h>\nint main(){\nint i;\nprintf(\"Hello Boss\\n\");\n}");
 	fclose(f);
-	system("gcc -o test test.c > yzsa");
-	system("./test > result");
-	f=fopen("result","r");
-	while(1){
-		fgets(s,256,f);
-		if(feof(f))
-			break;
-		printf("%s",s);
+}
+
+int buildFile(char* sourceFileName, char* errorLogFileName){
+	char command[50];
+	strcpy(command, "gcc -o bot ");
+	strcat(command, sourceFileName);
+	strcat(command, " 2>>");
+	strcat(command, errorLogFileName); //command = gcc -o a /*fileName*/ 2>> /*errorLogFileName*/
+	if(system(command)==256){
+		return -1; //build error, error mess in errorLogFileName
+	}
+	return 1; //build success
+}
+
+int runFile(char* fileName, char* errorLogFileName){
+	char command[50];
+	strcpy(command, "./bot >run.out 2>>");
+	strcat(command, errorLogFileName);
+	return system(command);
+}
+
+void createBotDirectory(char* errorLogFileName){
+	char command[50];
+	strcpy(command, "mkdir codes 2>>");
+	strcat(command, errorLogFileName);
+	system(command);
+}
+
+int main(){
+	char fileName[20];
+	char errorLogFileName[20];
+	createBotDirectory(errorLogFileName);
+	createFileName(fileName, 1);
+	genFile(fileName);
+	if(buildFile(fileName, errorLogFileName)!=-1){
+		runFile(fileName, errorLogFileName);
+	}else{
+		printf("Build error\n");
 	}
 }
